b98be9387135f92098782afffc6fd3fdecb5149c
honam <75104291+honambsn@users.noreply.github.com>
2025-06-30 21:55:51 +0700

    getting started

---
+++ BulkyWeb/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//update this for hot reload
+//builder.Services.AddControllersWithViews();
+
+
+if (builder.Environment.IsDevelopment())
+{
+    // For development, use the default controller with views
+    builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+}
+else
+{
+    // For production, use a minimal API approach
+    builder.Services.AddControllersWithViews();
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
