e8c1fc218d35a452020f29bed8055fe0a5c16ace
honam <75104291+honambsn@users.noreply.github.com>
2025-07-04 22:08:29 +0700

    update: create page

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -17,36 +17,11 @@ namespace BulkyWeb.Controllers
         {
             List<Category> objCategoryList = _db.Categories.ToList();
 
-            ///return View(objCategoryList);
-            ///
-            // Logic to decide the theme based on the time of day
-            var currentHour = DateTime.Now.Hour;
-            string navbarClass;
-            string textClass;
+            return View(objCategoryList);
+        }
 
-            var check = true;
-            // Set theme based on the time of day (Night: 6 PM to 6 AM, Day: 6 AM to 6 PM)
-            //if (currentHour >= 18 || currentHour < 6) // Nighttime (Dark Mode)
-            if(check)
-            {
-                navbarClass = "navbar-dark bg-primary";
-                textClass = "";  // Remove text-dark class
-            }
-            else // Daytime (Light Mode)
-            {
-                navbarClass = "navbar-light bg-white";
-                textClass = "text-dark";  // Add text-dark class
-            }
-
-            Debug.WriteLine("Navbar Class: " + navbarClass);
-            Debug.WriteLine("Text Class: " + textClass);
-
-
-            // Passing data to the View
-            ViewBag.NavbarClass = navbarClass;
-            ViewBag.TextClass = textClass;
-            ViewBag.Categories = objCategoryList;
-
+        public IActionResult Create()
+        {
             return View();
         }
     }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -6,8 +8,12 @@ namespace BulkyWeb.Models
     {
         [Key]
         public int ID { get; set; }
-        [Required]
+        [DisplayName("Category Name")]
+        [Required(ErrorMessage = "Category Name is required.")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Required(ErrorMessage = "Display Order is required.")]
+        [Range(0, 100, ErrorMessage = "Display Order must be between 0 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
