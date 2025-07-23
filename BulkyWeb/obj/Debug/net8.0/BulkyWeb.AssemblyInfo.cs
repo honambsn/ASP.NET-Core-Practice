b8a66568fc414c4ec1e579ed7c5c71841a78327f
honam <75104291+honambsn@users.noreply.github.com>
2025-07-23 21:49:28 +0700

    update: product crud

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312515a7eacd1ee8f8e5cf105186c4fda25b9e72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5601b20f96fb20a0e7f56a90186d692556cabf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
