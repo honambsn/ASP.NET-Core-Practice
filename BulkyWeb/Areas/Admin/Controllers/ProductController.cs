b7051220bcc75cc8e6fd5d2ce6b8d0f6c00d3cbe
honam <75104291+honambsn@users.noreply.github.com>
2025-08-05 21:29:06 +0700

    update: product crud. Bug: datatable pagination styling

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -297,7 +297,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return Json(new { data = objProductList });
         }
 
-        
+        [HttpDelete]
         public IActionResult Delete(int? id)
         {
             var productToBeDeleted = _unitOfWork.Product.Get(u => u.ID == id);
--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,9 +1,11 @@
-﻿$(document).ready(function () {
+﻿var dataTable;
+
+$(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    /*dataTable = */
+    dataTable =
     $('#tblData').DataTable({
         ajax: {
             url: '/admin/product/getall'
@@ -30,7 +32,7 @@ function loadDataTable() {
                     //    </div>`;
                     return `<div class="w-100 btn-group" role="group">
                         <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                        <a href="/admin/product/delete/${data}" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                        <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>
                     `
                 }
@@ -42,5 +44,34 @@ function loadDataTable() {
         lengthMenu: [5, 10, 25, 50],
         paging: true,
         info: true,
+    });
+}
+
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            //Swal.fire({
+            //    title: "Deleted!",
+            //    text: "Your file has been deleted.",
+            //    icon: "success"
+            //});
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
     });
 }
