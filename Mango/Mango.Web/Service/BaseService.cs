5ab506d60eb003c2b1cf1613dd29616bcf8add65
honam <75104291+honambsn@users.noreply.github.com>
2025-11-06 21:32:31 +0700

    wip: web prj

---
+++ Mango/Mango.Web/Service/BaseService.cs
@@ -0,0 +1,96 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseDTO?> SendAsync(RequestEDTO requestDTO)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+
+                //token
+                message.RequestUri = new Uri(requestDTO.URL);
+                if (requestDTO.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDTO.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDTO.APITYype)
+                {
+                    case APIType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case APIType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case APIType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.InternalServerError:
+                        return new()
+                        {
+                            IsSuccess = false,
+                            Message = " Internal Server Error"
+                        };
+                    case HttpStatusCode.Unauthorized:
+                        return new()
+                        {
+                            IsSuccess = false,
+                            Message = "Unauthorized"
+                        };
+                    case HttpStatusCode.Forbidden:
+                        return new()
+                        {
+                            IsSuccess = false,
+                            Message = "Access Denied"
+                        };
+                    case HttpStatusCode.NotFound:
+                        return new()
+                        {
+                            IsSuccess = false,
+                            Message = "Not Found",
+                        };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDTO = JsonConvert.DeserializeObject<ResponseDTO>(apiContent);
+                        return apiResponseDTO;
+                }
+            }
+            catch (Exception ex) {
+                var dto = new ResponseDTO()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+                return dto;
+            }
+
+        }
+    }
+}
---
+++ Mango/Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDTO?> GetCouponAsync(string couponCode);
+        Task<ResponseDTO?> GetAllCouponsAsync();
+        Task<ResponseDTO?> GetCouponByIDAsync(int id);
+        Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO);
+        Task<ResponseDTO?> DeleteCouponsAsync(int id);
+    }
+}
