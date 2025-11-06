595f672c0f8c85437b428c12c190ecc44924028d
honam <75104291+honambsn@users.noreply.github.com>
2025-11-06 21:32:31 +0700

    wip: web prj

---
+++ Mango/Mango.Web/Models/RequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestEDTO
+    {
+        public APIType APITYype { get; set; } = APIType.GET;
+        public string URL { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Mango/Mango.Web/Service/CouponService.cs
@@ -0,0 +1,44 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponByIDAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
