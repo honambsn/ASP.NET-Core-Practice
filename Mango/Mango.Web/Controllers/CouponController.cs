b3cdf57b7674b6334e54e7130cab90b29361c617
honam <75104291+honambsn@users.noreply.github.com>
2025-11-11 21:48:18 +0700

    wip: couponcontroller

---
+++ Mango/Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        //public async Task<IActionResult> CouponIndex()
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
