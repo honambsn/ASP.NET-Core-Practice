1a6bfa8136ab4e3bead0ff29d11d94a3aea0365c
honam <75104291+honambsn@users.noreply.github.com>
2025-11-24 21:19:12 +0700

    wip: authservice

---
+++ Mango/Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.DTOs;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegisterationRequestDTO registerationRequestDTO);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+    }
+}
