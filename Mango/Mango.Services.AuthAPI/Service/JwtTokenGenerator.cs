91d41d5caf5b1cd4f0d108355fc472a5a4500d37
honam <75104291+honambsn@users.noreply.github.com>
2025-12-01 21:35:04 +0700

    wip: jwt

---
+++ Mango/Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JWTOptions _jwtOptions;
+        public JwtTokenGenerator(JWTOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            var claimsList = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email, applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, applicationUser.UserName),
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimsList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials( new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
