694aefc74fbb9905bda79969fd04e4cdd8c8f666
honam <75104291+honambsn@users.noreply.github.com>
2025-12-01 21:35:04 +0700

    wip: jwt

--- Mango/Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango/Mango.Services.AuthAPI/Service/AuthService.cs
@@ -19,9 +19,38 @@ namespace Mango.Services.AuthAPI.Service
             _roleManager = roleManager;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower());
+
+            bool isValid = await _userManager.CheckPasswordAsync(user, loginRequestDTO.Password);
+
+            if (user == null || isValid == false)
+            {
+                return new LoginResponseDTO()
+                {
+                    User = null,
+                    Token = "",
+                };
+            }
+
+            // if user was found, gen jwt token
+
+            UserDTO userDTO = new()
+            {
+                Email = user.Email,
+                ID = user.Id,
+                Name = user.Name,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                User = userDTO,
+                Token = ""
+            };
+
+            return loginResponseDTO;
         }
 
         public async Task<string> Register(RegisterationRequestDTO registerationRequestDTO)
