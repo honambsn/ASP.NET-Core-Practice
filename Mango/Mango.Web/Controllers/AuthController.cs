3d245073b17d7008a4f2e8536ad7477021e14cc1
honam <75104291+honambsn@users.noreply.github.com>
2025-12-08 21:26:29 +0700

    wip: authcontroller

---
+++ Mango/Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO();
+            return View(loginRequestDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
