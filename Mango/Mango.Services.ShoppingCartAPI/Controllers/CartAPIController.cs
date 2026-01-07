72131291c74d37b93ca4a32c8418a2cd4fb67ee2
honam <75104291+honambsn@users.noreply.github.com>
2026-01-07 21:31:38 +0700

    wip: shoppingcartapi

---
+++ Mango/Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Mango.Services.ShoppingCartAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDTO _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+
+        public CartAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+            _db = db;
+            this._response = new ResponseDTO();
+            _mapper = mapper;
+        }
+
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDTO> CartUpsert(CartDTOs cartDTO)
+        {
+
+        }
+    }
+}
---
+++ Mango/Mango.Services.ShoppingCartAPI/Models/DTOs/CartHeaderDTOs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ShoppingCartAPI.Models.DTOs
+{
+    public class CartHeaderDTOs
+    {
+        public int CartHeaderID { get; set; }
+        public string? UserID { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
